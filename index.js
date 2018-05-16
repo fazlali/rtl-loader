2d1d2729866bcdbd18f02909cb8fdc262feaf4b3
Amin Fazlali <amin@fazlali.net>
2018-05-16 17:18:23 +0430

    bug fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function (style) {
     return style.replace(/{([^{}]*)}/gi, function(match, style){
-        var rtl_style = style.replace(/left/gi, '**rtl-css-left-replace**')
+        var rtl_style = style.replace(/left/gi, '##rtl-css-left-replacer##')
             .replace(/right/gi, 'left')
-            .replace('**rtl-css-left-replace**', 'right')
+            .replace(/##rtl-css-left-replacer##/gi, 'right')
             .replace(/([;^][\s\n]*border-radius[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*[$;])/gi,'$1 $3 $2 $5 $4 $7')
             .replace(/([;^][\s*\n](padding|margin)[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*[$;])/gi,'$1 $3 $6 $5 $4 $7')
             .replace(/(translateX)\(-([^)]+)\)/gi,'$1(+$2)')
