f6378de5d146eb7470eb1f458bff8828cb735a14
amin <amin@fazlali.net>
2021-07-04 15:29:38 +0430

    bug fixes

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 module.exports = function (style) {
-    return style.replace(/{([^{}]*)}/gi, function(match, style){
-        var rtl_style = style.replace(/left/gi, '##rtl-css-left-replacer##')
+    return style.replace(/{([^{}]*)}/gi, (_, style) => {
+        const rtl_style = style.replace(/left/gi, '##RTL_CSS_LEFT_REPLACER##')
             .replace(/right/gi, 'left')
-            .replace(/##rtl-css-left-replacer##/gi, 'right')
-            .replace(/([;^][\s\n]*border-radius[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*[$;])/gi,'$1 $3 $2 $5 $4 $7')
-            .replace(/([;^][\s*\n](padding|margin)[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*[$;])/gi,'$1 $3 $6 $5 $4 $7')
-            .replace(/(translateX)\(-([^)]+)\)/gi,'$1(+$2)')
-            .replace(/(translateX)\(([^-+)]+)\)/gi,'$1(-$2)')
+            .replace(/##RTL_CSS_LEFT_REPLACER##/gi, 'right')
+            .replace(/((;|^)[\s;\n]*border-radius[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*($|;))/gi,'$1 $4 $3 $6 $5 $8')
+            .replace(/((;|^)[\s;\n]*(padding|margin)[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*($|;))/gi,'$1 $4 $7 $6 $5 $8')
+            .replace(/translateX\(([^)]+)\)/gi, (match) => {
+                let value = match[1].trim()
+                if (value.charAt(0) === '-') {
+                    value = value.substr(1)
+                } else {
+                    value = value.replace(/^\+?/, '-')
+                }
+                return `translateX(${value})`
+            })
+            .replace(/translate\(([^),]+),([^),]+)\)/gi, (match) => {
+                let value = match[1].trim()
+                if (value.charAt(0) === '-') {
+                    value = value.substr(1)
+                } else {
+                    value = value.replace(/^\+?/, '-')
+                }
+                return `translate(${value}, ${match[2]})`
+            })
         ;
-        return '{' + rtl_style + '}';
+        return `{\n${rtl_style}\n}`;
     });
 };
