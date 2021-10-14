3d612dd167281b3118c5e655e4b1dfe54f45c936
amin <amin@fazlali.net>
2021-10-14 15:46:35 +0330

    bug fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function (style) {
     return style.replace(/{([^{}]*)}/gi, (_, style) => {
-        const rtl_style = style.replace(/left/gi, '##RTL_CSS_LEFT_REPLACER##')
-            .replace(/right/gi, 'left')
+        const rtl_style = style.replace(/(^|[^a-zA-Z])left($|[^a-zA-Z])/gi, '$1##RTL_CSS_LEFT_REPLACER##$2')
+            .replace(/(^|[^a-zA-Z])right($|[^a-zA-Z])/gi, '$1left$2')
             .replace(/##RTL_CSS_LEFT_REPLACER##/gi, 'right')
             .replace(/((;|^)[\s;\n]*border-radius[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*($|;))/gi,'$1 $4 $3 $6 $5 $8')
             .replace(/((;|^)[\s;\n]*(padding|margin)[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*($|;))/gi,'$1 $4 $7 $6 $5 $8')
