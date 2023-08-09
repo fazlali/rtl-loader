6f6a6ccdb04faadf43bba305bbb96fcac1c3a94e
fazlali <aminfa92@gmail.com>
2023-08-09 17:45:40 +0330

    Bug fix

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ module.exports = function (style) {
             .replace(/##RTL_CSS_LEFT_REPLACER##/gi, 'right')
             .replace(/((;|^)[\s;\n]*border-radius[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*($|;))/gi,'$1 $4 $3 $6 $5 $8')
             .replace(/((;|^)[\s;\n]*(padding|margin)[^:]*:\s*)([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)\s+([^;\s]+)([\s\n]*($|;))/gi,'$1 $4 $7 $6 $5 $8')
-            .replace(/translateX\(([^)]+)\)/gi, (match) => {
-                let value = match[1].trim()
-                if (value.charAt(0) === '-') {
-                    value = value.substr(1)
+            .replace(/translateX\(([^)]+)\)/gi, (match, x) => {
+                x = x.trim()
+                if (x.charAt(0) === '-') {
+                    x = x.substr(1)
                 } else {
-                    value = value.replace(/^\+?/, '-')
+                    x = x.replace(/^\+?/, '-')
                 }
-                return `translateX(${value})`
+                return `translateX(${x})`
             })
-            .replace(/translate\(([^),]+),([^),]+)\)/gi, (match) => {
-                let value = match[1].trim()
-                if (value.charAt(0) === '-') {
-                    value = value.substr(1)
+            .replace(/translate\(([^),]+),([^),]+)\)/gi, (match, x, y) => {
+                console.log(match, x, y)
+                x = x.trim()
+                if (x.charAt(0) === '-') {
+                    x = x.substr(1)
                 } else {
-                    value = value.replace(/^\+?/, '-')
+                    x = x.replace(/^\+?/, '-')
                 }
-                return `translate(${value}, ${match[2]})`
+                console.log(`translate(${x}, ${y})`)
+                return `translate(${x}, ${y})`
             })
         ;
         return `{${rtl_style}}`;
